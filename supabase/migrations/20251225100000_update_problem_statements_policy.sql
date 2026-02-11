e102771269b5bd32f8607a2d7941703fbf2dd11e
Vivek-Vardhan-310 <vivekvardhan310@gmail.com>
2026-02-11 13:09:02 +0530

    deleted files

--- supabase/migrations/20251225100000_update_problem_statements_policy.sql
+++
@@ -1,8 +0,0 @@
--- Update the policy to allow both anonymous and authenticated users to view problem statements
-DROP POLICY IF EXISTS "Anyone can view problem statements" ON public.problem_statements;
-
-CREATE POLICY "Anyone can view problem statements"
-ON public.problem_statements
-FOR SELECT
-TO anon, authenticated
-USING (true);
